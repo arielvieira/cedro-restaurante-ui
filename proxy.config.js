a87bee6a64f78e3d3354f5a53854d8e7d2fa5847
Ariel Vieira <arielvieirasv@gmail.com>
2018-05-01 13:42:58 -0300

    Update proxy.config

--- proxy.config.js
+++ proxy.config.js
@@ -1,9 +1,7 @@
-const proxy = [
-    {
-      context: '/api',
-      target: 'http://localhost:51270/api',
-      pathRewrite: {'^/api' : ''}
-    }
-  ];
+const proxy = [{
+  context: '/api',
+  target: 'http://localhost:51270/api',
+  pathRewrite: { '^/api': '' }
+}];
 
 module.exports = proxy;
